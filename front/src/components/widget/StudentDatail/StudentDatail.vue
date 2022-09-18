f874d2a89cb825c9cdaf672c154332861c86db71
HAK-KIM <hak.kim@student.passerellesnumeriques.org>
2022-09-18 13:35:20 +0700

    #48 When the teacher/coordinator want to comment on the follow up, there will be a card to display their comments.

---
+++ front/src/components/widget/StudentDatail/StudentDatail.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="w-2/5 border-l-2 px-5">
+            <div class="flex flex-col">
+              <div class="font-semibold text-xl py-4">Student Detail</div>
+              <img
+                src="https://source.unsplash.com/_7LbC5J-jw4/600x600"
+                class="object-cover rounded-full h-60 w-60"
+                alt=""
+              />
+              <div class="font-semibold py-4">
+                <ul>
+                  <li>First Name: MERN</li>
+                  <li>Last Name: Stack</li>
+                  <li>Class: WEB-A</li>
+                  <li>Batch: 2022</li>
+                  <li>Province: Kompong Thom</li>
+                  <li>Gender: M</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
