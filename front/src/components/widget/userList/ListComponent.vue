7e451f8335d7af90688b8d14a10ba675d1b8e586
Samoulvann <samoulvann@gmail.com>
2022-09-22 15:57:14 +0700

    #58 Update the profile image of users

--- front/src/components/widget/userList/ListComponent.vue
+++ front/src/components/widget/userList/ListComponent.vue
@@ -1,180 +1,388 @@
 <template>
   <!-- TITLE OF PAGES -->
-  <div class=" shadow-sm bg-gray-50 sm:rounded-lg p-4 z-50 border-2 border-t-[#018ABD]">
+  <div
+    class="
+      shadow-sm
+      bg-gray-50
+      sm:rounded-lg
+      p-4
+      z-50
+      border-2 border-t-[#018ABD]
+    "
+  >
     <div class="py-1 pb-3">
-      <h2 class="text-gray-800 text-2xl font-bold text-center mb-2 uppercase">{{ title }}</h2>
+      <h2 class="text-gray-800 text-2xl font-bold text-center mb-2 uppercase">
+        {{ title }}
+      </h2>
     </div>
 
-      <!-- ___BUTTON CREATE USER__-->
-      <div class="flex justify-start items-center mb-2">
-          <Base_Button class="h-10 bg-[#018ABD] py-[6px] flex justify-start" @click="addUser">
-            <i class="mx-1">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
-              </svg>
-            </i>
-            <i class="text-white">
-              Create
-            </i>
-        </Base_Button>
- 
-        
-        <!-- BUTON DROP DOWN MENU  BY BATCH-->
-        <Base_DropDwon_Menu  :title="'Batches'" :lists="lists"/>
-        <!--  GET FILTER BY CLASS-->
-        <Base_DropDwon_Menu  :title="'Class'"/>
-        <!-- GET FILTER BY SEARCH -->
-        <BaseSearch  />
-      </div>
-      <div>
-        
-      </div>
-      <!--_____FORM DIALOG_______-->
-    <div class=" overflow-x-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex" v-if="openDialog">
+    <!-- ___BUTTON CREATE USER__-->
+    <div class="flex justify-start items-center mb-2">
+      <Base_Button
+        class="h-10 bg-[#018ABD] py-[6px] flex justify-start"
+        @click="addUser"
+      >
+        <i class="mx-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 text-white"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+            />
+          </svg>
+        </i>
+        <i class="text-white"> Create </i>
+      </Base_Button>
+
+      <!-- BUTON DROP DOWN MENU  BY BATCH-->
+      <Base_DropDwon_Menu :title="'Batches'" :lists="lists" />
+      <!--  GET FILTER BY CLASS-->
+      <Base_DropDwon_Menu :title="'Class'" />
+      <!-- GET FILTER BY SEARCH -->
+      <BaseSearch />
+    </div>
+    <div></div>
+    <!--_____FORM DIALOG_______-->
+    <div
+      class="
+        overflow-x-auto
+        fixed
+        inset-0
+        z-50
+        outline-none
+        focus:outline-none
+        justify-center
+        items-center
+        flex
+      "
+      v-if="openDialog"
+    >
       <div class="form-container rounded w-11/12">
-          <Bass_Dialog_Form class="z-50" @cancel="onCancelCreated" @close="openDialogs" :object="object" @create_student="emitPage" :updateValue="objectUpdating"></Bass_Dialog_Form>
+        <Bass_Dialog_Form
+          class="z-50"
+          @cancel="onCancelCreated"
+          @close="openDialogs"
+          :object="object"
+          @create_student="emitPage"
+          :updateValue="objectUpdating"
+        ></Bass_Dialog_Form>
       </div>
     </div>
 
-      <!-- CLOSE THE DIALOG -->
-     <div v-if="openDialog" class="opacity-30 fixed inset-0 z-40 bg-black"></div>
+    <!-- CLOSE THE DIALOG -->
+    <div v-if="openDialog" class="opacity-30 fixed inset-0 z-40 bg-black"></div>
     <!-- TABLES COMTAINER ALL LIST OF STUDENTS-->
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400" >
-      <thead class="text-xs text-gray-700 uppercase bg-slate-300 dark:bg-gray-50 dark:text-gray-400">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead
+        class="
+          text-xs text-gray-700
+          uppercase
+          bg-slate-300
+          dark:bg-gray-50 dark:text-gray-400
+        "
+      >
         <!-- ____HEADER OF TABLE LIST_____ -->
         <tr>
           <th scope="col" class="py-2 px-4 text-left">Name</th>
-          <th scope="col" class="py-2 px-4 text-center" v-if="standingPage=='teacher'" >Email</th> 
-          <th scope="col" class="py-2 px-4 text-center" v-if="standingPage=='student'">Batch</th>
-          <th scope="col" class="py-2 px-4 text-center" v-if="standingPage=='student'">class</th>
-          <th scope="col" class="py-2  text-center w-4 mx-4">Action</th>
+          <th
+            scope="col"
+            class="py-2 px-4 text-center"
+            v-if="standingPage == 'teacher'"
+          >
+            Email
+          </th>
+          <th
+            scope="col"
+            class="py-2 px-4 text-center"
+            v-if="standingPage == 'student'"
+          >
+            Batch
+          </th>
+          <th
+            scope="col"
+            class="py-2 px-4 text-center"
+            v-if="standingPage == 'student'"
+          >
+            class
+          </th>
+          <th scope="col" class="py-2 text-center w-4 mx-4">Action</th>
         </tr>
       </thead>
 
       <tbody class="overflow-right-auto" v-if="showListStd">
         <!-- __BODY LIST USER____-->
-        <tr v-for="(item,index) in listUsers" :key="index" class=" border-b dark:bg-gray-100  hover:bg-gray-50 dark:hover:bg-gray-300">
-          <td class="text-black font-semibold py-3 px-4 text-cente">{{ item.first_name }} {{ item.last_name }}</td>
-          <td class="py-3 px-4 text-center text-blue-500" v-if="standingPage=='teacher'">{{ item.email }}</td> <!-- Teacher email -->
-          <td class="py-3 px-4 text-center text-orange-300" v-if="(standingPage=='student')"> {{ item.student[0].year }}</td>
-          <td class="py-3 px-4 text-center text-blue-300" v-if="standingPage=='student'">{{ item.student[0].class }}</td>
-         <!-- ________GROUP BUTTON ACTIONS____________-->
-          <td  class="w-11/12 flex items-center justify-end mt-2">
-              <!-- Edite -->
-              <i class="group max-w-max relative mx-1 mt-2 flex flex-col items-center justify-center   hover:text-gray-600" v-on:click="editUser(item.id)">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mx-1 text-blue-500 cursor-pointe">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                </svg>
-                <div class="group-hover:[transform:perspective(0px)_translateZ(0)_rotateX(0deg)] absolute bottom-0 mb-6 origin-bottom transform rounded text-white opacity-0 transition-all duration-300 group-hover:opacity-100">
-                    <div class="flex max-w-xs flex-col items-center">
-                      <div class="rounded bg-gray-100  opacity-1 px-4 py-1 text-xs text-black text-center shadow-lg">Edit</div>
-                    </div>
+        <tr
+          v-for="(item, index) in listUsers"
+          :key="index"
+          class="
+            border-b
+            dark:bg-gray-100
+            hover:bg-gray-50
+            dark:hover:bg-gray-300
+          "
+        >
+          <td class="text-black font-semibold py-3 px-4 text-cente">
+            {{ item.first_name }} {{ item.last_name }}
+          </td>
+          <td
+            class="py-3 px-4 text-center text-blue-500"
+            v-if="standingPage == 'teacher'"
+          >
+            {{ item.email }}
+          </td>
+          <!-- Teacher email -->
+          <td
+            class="py-3 px-4 text-center text-orange-300"
+            v-if="standingPage == 'student'"
+          >
+            {{ item.student[0].year }}
+          </td>
+          <td
+            class="py-3 px-4 text-center text-blue-300"
+            v-if="standingPage == 'student'"
+          >
+            {{ item.student[0].class }}
+          </td>
+          <!-- ________GROUP BUTTON ACTIONS____________-->
+          <td class="w-11/12 flex items-center justify-end mt-2">
+            <!-- Edite -->
+            <i
+              class="
+                group
+                max-w-max
+                relative
+                mx-1
+                mt-2
+                flex flex-col
+                items-center
+                justify-center
+                hover:text-gray-600
+              "
+              v-on:click="editUser(item.id)"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-4 h-4 mx-1 text-blue-500 cursor-pointe"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                />
+              </svg>
+              <div
+                class="
+                  group-hover:[transform:perspective(0px)_translateZ(0)_rotateX(0deg)]
+                  absolute
+                  bottom-0
+                  mb-6
+                  origin-bottom
+                  transform
+                  rounded
+                  text-white
+                  opacity-0
+                  transition-all
+                  duration-300
+                  group-hover:opacity-100
+                "
+              >
+                <div class="flex max-w-xs flex-col items-center">
+                  <div
+                    class="
+                      rounded
+                      bg-gray-100
+                      opacity-1
+                      px-4
+                      py-1
+                      text-xs text-black text-center
+                      shadow-lg
+                    "
+                  >
+                    Edit
+                  </div>
                 </div>
-              </i>
-              
-              <!-- delete  -->
-              <i class="group max-w-max relative mx-1 mt-2 flex flex-col items-center justify-center   hover:text-gray-600" v-if="standingPage !='follow'" @click="($emit('delete_id', item.id))">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-red-500 cursor-pointer">
-                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                </svg> 
+              </div>
+            </i>
 
-                <div class="group-hover:[transform:perspective(0px)_translateZ(0)_rotateX(0deg)] absolute bottom-0 mb-6 origin-bottom transform rounded text-white opacity-0 transition-all duration-300 group-hover:opacity-100">
-                    <div class="flex max-w-xs flex-col items-center">
-                      <div class="rounded bg-gray-100  opacity-1 px-2 py-1 text-xs text-black text-center shadow-lg">Delete</div>
-                    </div>
+            <!-- delete  -->
+            <i
+              class="
+                group
+                max-w-max
+                relative
+                mx-1
+                mt-2
+                flex flex-col
+                items-center
+                justify-center
+                hover:text-gray-600
+              "
+              v-if="standingPage != 'follow'"
+              @click="$emit('delete_id', item.id)"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-4 h-4 text-red-500 cursor-pointer"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
+
+              <div
+                class="
+                  group-hover:[transform:perspective(0px)_translateZ(0)_rotateX(0deg)]
+                  absolute
+                  bottom-0
+                  mb-6
+                  origin-bottom
+                  transform
+                  rounded
+                  text-white
+                  opacity-0
+                  transition-all
+                  duration-300
+                  group-hover:opacity-100
+                "
+              >
+                <div class="flex max-w-xs flex-col items-center">
+                  <div
+                    class="
+                      rounded
+                      bg-gray-100
+                      opacity-1
+                      px-2
+                      py-1
+                      text-xs text-black text-center
+                      shadow-lg
+                    "
+                  >
+                    Delete
+                  </div>
                 </div>
-              </i>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <!--______IF NON LIST HERE_______-->
-    <div v-if="!showListStd" class="w-full  dark:bg-gray-800  dark:hover:bg-gray-600 flex justify-center items-center py-4">
+              </div>
+            </i>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <!--______IF NON LIST HERE_______-->
+    <div
+      v-if="!showListStd"
+      class="
+        w-full
+        dark:bg-gray-800 dark:hover:bg-gray-600
+        flex
+        justify-center
+        items-center
+        py-4
+      "
+    >
       <h1 class="text-red-600 text-[20px]">List User is empty</h1>
     </div>
-
   </div>
 </template>
 <script>
-import axios  from 'axios'
-import Base_Button from '../button/BaseButton.vue'
-import Bass_Dialog_Form from '../dialogFrom/BaseDialogForm.vue'
-import Base_DropDwon_Menu from '../dropdown_menu/BaseDropDown.vue'
-import BaseSearch from '../search/BaseSearch.vue'
+import axios from "axios";
+import Base_Button from "../button/BaseButton.vue";
+import Bass_Dialog_Form from "../dialogFrom/BaseDialogForm.vue";
+import Base_DropDwon_Menu from "../dropdown_menu/BaseDropDown.vue";
+import BaseSearch from "../search/BaseSearch.vue";
 export default {
-    props:['listUsers', 'createUsers', 'updateUser', 'title','standingPage'], 
-    emits:['emits-page', 'set_to_sfu'],
-    components: {
-       Bass_Dialog_Form, 
-       Base_Button, 
-       Base_DropDwon_Menu,
-       BaseSearch
-    },
-    data(){
-      return {
-          openDialog: false,
-          object:{},
-          objectUpdating:{}, // Store list which should we update
-          lists: [2022, 2023, 2024],
-          showListStd: false,
-        }
+  props: ["listUsers", "createUsers", "updateUser", "title", "standingPage"],
+  emits: ["emits-page", "set_to_sfu"],
+  components: {
+    Bass_Dialog_Form,
+    Base_Button,
+    Base_DropDwon_Menu,
+    BaseSearch,
+  },
+  data() {
+    return {
+      openDialog: false,
+      object: {},
+      objectUpdating: {}, // Store list which should we update
+      lists: [2022, 2023, 2024],
+      showListStd: false,
+    };
   },
   watch: {
     listUsers() {
-          this.showListStd = this.listUsers.length>0
-          this.listUsers
-    } 
+      this.showListStd = this.listUsers.length > 0;
+      this.listUsers;
+    },
   },
   methods: {
     // ADD USER THE DATABASE
     addUser() {
       this.openDialog = !this.openDialog;
-      this.object = this.createUsers
+      this.object = this.createUsers;
     },
 
-    // UPDATE DATA 
+    // UPDATE DATA
     editUser(userId) {
       this.openDialog = !this.openDialog;
-      this.object = this.updateUser
-      for(var i = 0; i < this.listUsers.length; i++){
-        if(this.listUsers[i].id == userId){
-          this.objectUpdating = this.listUsers[i]
+      this.object = this.updateUser;
+      for (var i = 0; i < this.listUsers.length; i++) {
+        if (this.listUsers[i].id == userId) {
+          this.objectUpdating = this.listUsers[i];
         }
       }
-      this.object.id = userId
+      this.object.id = userId;
     },
 
     // SHOWING CANCEL
-    onCancelCreated(isShow){
-      this.openDialog = isShow
-    },
-   // OPEN THE DIALOG
-    openDialogs(isShow){
+    onCancelCreated(isShow) {
       this.openDialog = isShow;
-      this.$emit('refresh_data')
+    },
+    // OPEN THE DIALOG
+    openDialogs(isShow) {
+      this.openDialog = isShow;
+      this.$emit("refresh_data");
     },
     // EMIT PAGE OT DATABASE
-    emitPage(value){
-      return this.$emit('emits-page',value);
+    emitPage(value) {
+      return this.$emit("emits-page", value);
     },
-      // ADD STUDENT TO FOLLO UP LIST
-      addToFollupList(id){
-      for(var i = 0; i < this.listUsers.length; i++){
-        if(this.listUsers[i].id == id){
+    // ADD STUDENT TO FOLLO UP LIST
+    addToFollupList(id) {
+      for (var i = 0; i < this.listUsers.length; i++) {
+        if (this.listUsers[i].id == id) {
           // this.objectUpdating = this.listUsers[i]
           // let data = this.this.objectUpdatings[i].student[0].if_follow_up = 'Yes';
-          axios.post('http://127.0.0.1:8000/api/follow_up/'+id, {if_follow_up: 'Yes'} ).then(()=>{
-            console.log("Set sucess successfully");
-          })
+          axios
+            .post("http://127.0.0.1:8000/api/user/2" + id, {
+              if_follow_up: "Yes",
+            })
+            .then(() => {
+              console.log("Set sucess successfully");
+            });
         }
       }
-      }
+    },
   },
   mounted() {
     if (this.listUsers.length > 0) {
-        this.showListStd = true
+      this.showListStd = true;
     }
-  }
+  },
 };
 </script>
 
--- front/src/views/coordinators/ProfileView.vue
+++ front/src/views/coordinators/ProfileView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full mt-24 m-auto px-8">
     <!-- component -->
-    <div class="bg-gray-100">
+    <div  class="bg-gray-100">
       <div class="w-full text-white border-t-4 border-blue-300">
         <div
           x-data="{ open: false }"
@@ -18,6 +18,7 @@
             <a
               href="#"
               class="
+                
                 text-[3rem]
                 text-blue-500
                 font-semibold
@@ -40,7 +41,7 @@
                 <path
                   x-show="!open"
                   fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-  1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
                   clip-rule="evenodd"
                 ></path>
                 <path
@@ -67,65 +68,58 @@
         </div>
       </div>
       <!-- End of Navbar -->
-      <div class="container mx-auto p-5">
+      <div v-for="teacher of ListTeacher" :key="teacher" class="container mx-auto p-5">
         <div class="md:flex no-wrap">
           <!-- Left Side -->
           <div class="w-full md:w-2/12 md:mx-8">
             <!-- Profile Card -->
             <div class="bg-white p-3 border-t-2 border-green-400">
               <div class="image overflow-hidden">
-                <img
+                <!-- <img
                   class="h-auto w-full mx-auto"
                   src="@/assets/samol.jpg"
                   alt=""
-                />
+                /> -->
+                
+<img class="max-w-full h-auto" src="@/assets/savert.jpg" alt="image description">
+
               </div>
-              <div class="flex justify-center  border-rounded">
-                <div class="image">
-                  <img
-                    class="h-auto w-1/5 mx-auto cursor-pointer mt-[-20px]"
-                    src="@/assets/cam.png"
-                    alt=""
-                  />
+              <div  class="flex justify-center  border-rounded">
+                <div  class="image">
+                  <input @change="tageImage" id="profile-upload" type="file" accept="image/*" hidden>
+                  <label for="profile-upload">
+                    <img
+                    
+                      :for="isUpload? 'profile-upload': 'not-puload'"  @click="hadleUpload"
+                      class="h-auto w-1/5 mx-auto cursor-pointer mt-[-20px]"
+                      src="@/assets/cam.png"
+                      alt=""
+                    />
+
+                  </label>
+                  
+
                 </div>
               </div>
               <div
                 class="
                   flex
-                  items-center
+                  justify-center
                   space-x-2
                   font-semibold
                   text-gray-800
                   leading-8
                 "
               >
-                <span clas="text-green-500">
-                  <svg
-                    class="h-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                    />
-                  </svg>
-                </span>
                 <h1
                   class="
-                    flex
-                    justify-center
                     text-gray-900
                     font-bold
-                    text-xl
+                    text-xl   
                     my-4
                   "
                 >
-                  Samoul Vann
+                {{teacher.first_name}} {{teacher.last_name}}
                 </h1>
               </div>
             </div>
@@ -135,8 +129,8 @@
           <div  class="w-full md:w-9/12">
             <!-- Profile tab -->
             <!-- About Section -->
-            <div class="bg-white p-4 shadow-sm rounded-sm">
-              <div
+            <div   class="bg-white p-4 shadow-sm rounded-sm">
+              <div 
                 class="
                   flex
                   items-center
@@ -164,40 +158,25 @@
                 </span>
                 <span class="tracking-wide">About</span>
               </div>
-              <div   class="text-gray-700 border-t-2 border-gray-300">
+              <div class=" text-gray-700 border-t-2 border-gray-300">
                 <div class="grid md:grid-cols-2 text-sm">
-                  <div class="grid grid-cols-2">
+                  <div  class="grid grid-cols-2">
                     <div class="px-4 py-2 font-semibold">First Name</div>
-                    <div class="px-4 py-2">Smoul</div>
+                    <div class="px-4 py-2">{{teacher.first_name}}</div>
                   </div>
                   <div class="grid grid-cols-2">
                     <div class="px-4 py-2 font-semibold">Last Name</div>
-                    <div class="px-4 py-2">Doe</div>
+                    <div class="px-4 py-2">{{teacher.last_name}}</div>
                   </div>
-                  <div class="grid grid-cols-2">
-                    <div class="px-4 py-2 font-semibold">Class</div>
-                    <div class="px-4 py-2">WEB B</div>
-                  </div>
-
-                  <div class="grid grid-cols-2">
-                    <div class="px-4 py-2 font-semibold">Batch</div>
-                    <div class="px-4 py-2">2022</div>
-                  </div>
+      
                   <div class="grid grid-cols-2">
                     <div class="px-4 py-2 font-semibold">Gender</div>
                     <div class="px-4 py-2">Female</div>
                   </div>
-                  <div class="grid grid-cols-2">
-                    <div class="px-4 py-2 font-semibold">Provence</div>
-                    <div class="px-4 py-2">Banteay Mean Chey</div>
-                  </div>
-                  <div class="grid grid-cols-2">
-                    <div class="px-4 py-2 font-semibold">NGO_Name</div>
-                    <div class="px-4 py-2">Banteay Mean Chey</div>
-                  </div>
-                  <div class="grid grid-cols-2">
-                    <div class="px-4 py-2 font-semibold">Birthday</div>
-                    <div class="px-4 py-2">Feb 06, 1998</div>
+
+                  <div class=" grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Email</div>
+                    <div class="px-4 py-2">{{teacher.email}}</div>
                   </div>
                 </div>
               </div>
@@ -209,11 +188,48 @@
   </div>
 </template>
 <script>
+  import axios from "axios";
 export default {
   data() {
     return {
+      ListTeacher:[],
+      profile:"",
+      image:null,
+      isUpload:false,
     };
   },
+  methods:{
+    teacherFromAPI(){
+      axios.get("http://127.0.0.1:8000/api/user/1").then((res)=>{
+        this.ListTeacher = res   .data;
+      })
+    },
+    tageImage(event){
+      this.image = event.target.files[0];
+      this.profile = URL.createObjectURL(event.target.files[0]);
+    },
+    hadleUpload() {
+      if(this.isUpload){
+        let userProfile = new FormData();
+        userProfile.append("image", this.image);
+        userProfile.append("_method", 'PUT');
+        axios.post("user" + this.user.id, userProfile)
+        .then(() => {console.log("Update image successfully")});
+        this.clickChangeprofile=!this.clickChangeprofile;
+      }
+      this.isUpload = !this.isUpload;
+    },
+    clearUploadImage() {
+      this.image=null;
+      this.profile='http://127.0.0.1:8000/storage/pictures/'+this.user.image
+      this.isUpload=false;
+      this.clickChangeprofile=!this.clickChangeprofile;
+    },
+
+    },
+    mounted(){
+      this.teacherFromAPI();
+    }
  
 };
 </script>
