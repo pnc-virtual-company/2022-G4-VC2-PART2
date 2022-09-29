275e0536e0ddba8b18a2cd4036549e27cd8575bf
Ulvy2022 <ulvy.romy@student.passerellesnumeriques.org>
2022-09-29 07:39:32 +0700

    #50

--- front/src/views/coordinators/FollowUpView.vue
+++ front/src/views/coordinators/FollowUpView.vue
@@ -94,7 +94,7 @@
                             <div class="flex justify-between flex-1 px-4 font-bold w-full">
                               {{capitalize(commets.user.first_name) }} {{capitalize(commets.user.last_name) }}
                         
-                              <div class=" flex justify-evenly">
+                              <div class=" flex justify-evenly ">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                                   class="w-6 h-6 mr-5 cursor-pointer" @click="deleteCmt(cmt.id)">
                                   <path stroke-linecap="round" stroke-linejoin="round"
@@ -362,7 +362,7 @@ export default {
       const userComment = {
         content: this.comment,
         topic: this.topic,
-        user_id: 3,
+        user_id: 1,
         student_id: studentID,
         follow_up_id: followUPID,
         action: this.action
